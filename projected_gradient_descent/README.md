328c7703ad0c2faef8319c4ab2c205f861a364e9
Tudor <tudor.batica@gmail.com>
2021-04-04 15:04:50 +0300

    add plots, update readme

--- projected_gradient_descent/README.md
+++ projected_gradient_descent/README.md
@@ -1,3 +1,9 @@
 # Projected Gradient Descent
 
 This folder contains a projected gradient descent demo.  
+The .py file solves the following problem:  
+
+`min_x (x1 - 2)^4 + (x1 - 2x2)^2  
+s.l. x in Q = {||x|| <= 2}`  
+
+and plots how the gradient converges.
--- projected_gradient_descent/projected_gradient.py
+++ projected_gradient_descent/projected_gradient.py
@@ -4,160 +4,90 @@ from math import sqrt
 from numpy import linalg as la
 from scipy import optimize
 
-## min_x (x1 - 2)^4 + (x1 - 2x2)^2 
-## s.l. x in Q = {||x|| <= 2}
+# min_x (x1 - 2)^4 + (x1 - 2x2)^2
+# s.l. x in Q = {||x|| <= 2}
 
-epsilon = 10**-3
-
-starting_value = np.array([[1], [1]])
 
 def function(x: np.array) -> float:
     return (x[0][0] - 2)**4 + (x[0][0] - 2*x[1][0])**2
 
+
 def gradient(x: np.array) -> np.array:
     return np.array([[4*(x[0][0] - 2)**3 + 2*(x[0][0] - 2*x[1][0])], [-4*(x[0][0] - 2*x[1][0])]])
+
 
 def projection(x: np.array) -> np.array:
     return min(1, 2/(sqrt(x[0]**2 + x[1]**2))) * x
 
-def stop(fun_value: float, target_value: float, epsilon: float) -> bool:
-    return abs(fun_value - target_value) <= epsilon
 
-def const_projected_gradient_descent():
-    cons = ({'type': 'ineq', 'fun': lambda x:  -1 * sqrt(x[0]**2 + x[1]**2) + 2})
-    f_star = optimize.minimize(lambda x: (x[0] - 2)**4 + (x[0] - 2*x[1])**2, (1, 1), method='SLSQP', constraints=cons).x
+def stop(xk: np.array, epsilon: float) -> bool:
+    grad = gradient(xk)
+    return sqrt(grad[0][0]**2 + grad[1][0]**2) <= epsilon
 
-    print(f_star)
-    alpha = 0.1
-    new_x, old_x, steps = np.array([[1], [1]]), np.array([[1], [1]]), 0
-    while not stop(function(new_x), function(np.array([[f_star[0]], [f_star[1]]])), epsilon):
+
+epsilon = 10**-1
+cons = ({'type': 'ineq', 'fun': lambda x: -1 * sqrt(x[0]**2 + x[1]**2) + 2})
+f_star = optimize.minimize(lambda x: (
+    x[0] - 2)**4 + (x[0] - 2*x[1])**2, (1, 1), method='SLSQP', constraints=cons)
+target = function(np.array([[f_star.x[0]], [f_star.x[1]]]))
+
+
+def const_projected_gradient_descent(alpha: float = 0.1):
+    new_x, old_x, steps = projection(
+        np.array([[1], [1]])), projection(np.array([[1], [1]])), 0
+    gradient_norm_evolution = []
+    function_evolution = []
+    while not stop(new_x, epsilon):
         new_x = projection(old_x - alpha * gradient(old_x))
         old_x = new_x
+        grad = gradient(new_x)
+        gradient_norm_evolution.append(sqrt(grad[0][0]**2 + grad[1][0]**2))
+        function_evolution.append(abs(function(new_x) - target))
         steps += 1
-    return old_x, steps
+    return old_x, steps, gradient_norm_evolution, function_evolution
+
 
 def backtracking_projected_gradient_descent():
-    cons = ({'type': 'ineq', 'fun': lambda x:  -1 * sqrt(x[0]**2 + x[1]**2) + 2})
-    f_star = optimize.minimize(lambda x: (x[0] - 2)**4 + (x[0] - 2*x[1])**2, (1, 1), method='SLSQP', constraints=cons).x
-    
-    new_x, old_x, steps = np.array([[1], [1]]), np.array([[1], [1]]), 0
-    c, ro = 0.5, 0.5
-    while not stop(function(new_x), function(np.array([[f_star[0]], [f_star[1]]])), epsilon):
-        alpha = 1
+    new_x, old_x, steps = projection(
+        np.array([[1], [1]])), projection(np.array([[1], [1]])), 0
+    gradient_norm_evolution = []
+    function_evolution = []
+    c, ro = 0.5, 0.1
+    while not stop(new_x, epsilon):
+        alpha = 0.5
         new_x = projection(old_x - alpha * gradient(old_x))
         while function(new_x) > function(old_x) - (c/alpha) * ((new_x[0][0] - old_x[0][0])**2 - (new_x[1][0] - old_x[1][0])**2):
             alpha *= ro
             new_x = projection(old_x - alpha * gradient(old_x))
-            print(alpha)
         new_x = projection(old_x - alpha * gradient(old_x))
         old_x = new_x
+        grad = gradient(new_x)
+        gradient_norm_evolution.append(sqrt(grad[0][0]**2 + grad[1][0]**2))
+        function_evolution.append(abs(function(new_x) - target))
         steps += 1
-    return old_x, steps
-
-#print(function(np.array([[1.69], [0.84]])))
-#print(gradient(np.array([[2], [2]])))
+    return old_x, steps, gradient_norm_evolution, function_evolution
 
 
-print(f'constant step:\n{const_projected_gradient_descent()}')
-print(f'backtracked step:\n{backtracking_projected_gradient_descent()}')
+if __name__ == '__main__':
+    _, const_steps, const_gradient_norm_evolution, const_function_evolution = const_projected_gradient_descent()
+    _, back_steps, back_gradient_norm_evolution, back_function_evolution = backtracking_projected_gradient_descent()
 
-
-"""
-## Date: Q, q
-Q = np.array([[1,1],[1,1.02]])
-q = np.array([[2],[2]])
-a = np.array(np.random.rand(2,1))
-sol = -la.inv(Q)@q
-b = a.T@sol - np.array(np.random.rand(1,1))
-f = lambda x: 0.5*x.T@Q@x + q.T@x
-Proj = lambda x: x - (np.maximum(0,a.T@x - b)/(la.norm(a)**2))*a
-
-#### Rezolvare folosing CVXPY
-n = Q.shape[0]
-z = cp.Variable(n)
-objective = cp.Minimize(0.5*cp.quad_form(z,Q) + q.T@z)
-constraints = [a.T@z<=b]
-prob = cp.Problem(objective, constraints)
-result = prob.solve(solver='CVXOPT')
-sol = z.value        
-
-
-k = 0
-eps = 10e-4
-x00 = np.array(70*np.random.rand(2,1))
-x0 = Proj(x00)
-
-### Constanta Lipschitz a gradientului
-Lips = np.max(la.eigvals(Q))
-alpha = 1/Lips
-print(alpha)
-all_x_i = [x0[0]]
-all_y_i = [x0[1]]
-all_f_i = [f(x0)]
-x_old = x0
-x = Proj(x_old - alpha*(Q@x_old + q))
-all_x_i.append(x[0])
-all_y_i.append(x[1])
-all_f_i.append(f(x))
-criteriu_stop = la.norm(x - x_old)
-
-while (criteriu_stop > eps**2):
-    x_old = x
+    plt.plot([i for i in range(1, const_steps + 1)],
+             const_gradient_norm_evolution)
+    plt.plot([i for i in range(1, back_steps + 1)],
+             back_gradient_norm_evolution)
+    plt.ylabel("||gradient(xk)||")
+    plt.xlabel("k")
+    plt.legend(["Constant Step(alpha = 0.1)", "Backtracking Step"])
+    plt.show()
+    plt.savefig("gradient_evolution")
     
-    ## Pas gradient
-    grad = Q@x+q
-    y = x - alpha*grad
-    
-    ## Pas proiectie
-    x = Proj(y)
-    
-    #print(a.T@x-b)
-    criteriu_stop = la.norm(x - x_old)
-    k = k+1
-
-    all_x_i.append(x[0])
-    all_y_i.append(x[1])
-    all_f_i.append(f(x))
-
-solMGP = x      
-
-print("#######################")
-print("")
-print("Solutie gasita de MG: ",'(',solMGP[0],solMGP[1],')')
-print("Solutie optima: ",'(',sol[0],sol[1],')')
-print("Valoare optima: ",f(sol))
-print("")
-print("#######################")   
-#### Plot ####
-
-
-x, y = np.mgrid[-10:10:0.1, -10:10:0.1]
-x = x.T
-y = y.T
-
-fig = plt.figure(1, figsize=(4, 4))
-plt.clf()
-plt.axes([0, 0, 1, 1])
-
-fcont = 0.5*Q[0,0]*(x**2) + 0.5*Q[1,1]*(y**2) + Q[1,0]*x*y + q[0]*x+ q[1]*y
-contours = plt.contour(fcont, extent=[-10, 10, -10, 10],
-                    cmap=plt.cm.gnuplot)
-
-
-
-#Etichete pentru multimile izonivel
-
-plt.clabel(contours, inline=1,
-                fmt='%1.1f', fontsize=10)
-
-plt.plot(all_x_i, all_y_i, 'b-', linewidth=2)
-plt.plot(all_x_i, all_y_i, 'k+')
-plt.plot(sol[0], sol[1], 'rx', markersize = 14)
-
-
-
-
-
-fig.savefig('MGP_quad_cs.pdf')
-"""
-
+    plt.plot([i for i in range(1, const_steps + 1)],
+             const_function_evolution)
+    plt.plot([i for i in range(1, back_steps + 1)],
+             back_function_evolution)
+    plt.ylabel("||f(xk) - f*||")
+    plt.xlabel("k")
+    plt.legend(["Constant Step(alpha = 0.1)", "Backtracking Step"])
+    plt.show()
+    plt.savefig("difference_to_target_evolution")
