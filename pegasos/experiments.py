82071c9e1b1f2a9acd0fffc65c65f1de41a16ffc
Tudor <tudor.batica@gmail.com>
2021-05-20 16:42:09 +0300

    add plot

--- pegasos/experiments.py
+++ pegasos/experiments.py
@@ -1,7 +1,37 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from data import *
 from pegasos import *
+
+
+def loss_convergence_depending_on_batch_size(training_data, desired_loss):
+    """
+    Compares the numbers of iterations required to achieve the same loss, 
+    for different batch sizes.
+    """
+    batch_sizes = np.arange(2.5, 100, 2.5)
+    iters_required = []
+    for size in batch_sizes:
+        params = PegasosParameters(lmbda=1, batch_size=int(
+            len(training_data.data) * size / 100), iterations=10000000000, loss_threshold=desired_loss, verbose=False)
+        model = PegasosSolver(params)
+        iters_required.append(len(model.train(training_data)))
+        print(f'{size}% done...')
+    batch_sizes = [f'{size}%' for size in batch_sizes]
+
+    _plot_iterations_required(desired_loss, batch_sizes, iters_required)
+
+
+def _plot_iterations_required(desired_loss, k, iters):
+    plt.figure()
+    plt.title(f'Iterations required to obtain a loss of {desired_loss}')
+    plt.xlabel('Batch Size')
+    plt.ylabel('Iterations')
+    plt.xticks(rotation = 65)
+    plt.bar(k, iters)
+    plt.show()
+
 
 def compare_loss_convergence(desired_loss: float, training_data: LabeledData, mini_batch_size: int):
     """
@@ -9,16 +39,22 @@ def compare_loss_convergence(desired_loss: float, training_data: LabeledData, mi
     by plotting their loss functions.
     """
     mini_batch_params = PegasosParameters(lmbda=1, batch_size=mini_batch_size,
-                                   iterations=100000000, loss_threshold=desired_loss, verbose=False)
-    normal_params = PegasosParameters(lmbda=1, iterations=100000000, loss_threshold=desired_loss, verbose=False)
-    
+                                          iterations=100000000, loss_threshold=desired_loss, verbose=False)
+    normal_params = PegasosParameters(
+        lmbda=1, iterations=100000000, loss_threshold=desired_loss, verbose=False)
+
     mini_batch_model = PegasosSolver(mini_batch_params)
     normal_model = PegasosSolver(normal_params)
 
     mini_batch_loss = mini_batch_model.train(training_data)
     normal_loss = normal_model.train(training_data)
 
-    mini_batch_size_percentage =  100 * mini_batch_size / len(training_data.data)
+    _plot_losses_on_same_figure(mini_batch_loss, normal_loss, mini_batch_size)
+
+
+def _plot_losses_on_same_figure(mini_batch_loss, normal_loss, mini_batch_size):
+    mini_batch_size_percentage = 100 * \
+        mini_batch_size / len(training_data.data)
 
     plt.figure()
     plt.title(f'Loss convergence - desired loss = {desired_loss}')
@@ -26,7 +62,9 @@ def compare_loss_convergence(desired_loss: float, training_data: LabeledData, mi
     plt.plot(normal_loss, 'r+')
     plt.xlabel("Iteration")
     plt.ylabel("Loss")
-    plt.legend([f'Mini-Batch Pegasos(k={mini_batch_size}, i.e. {mini_batch_size_percentage:.1f}%)', 'Normal Pegasos'])
+    plt.legend(
+        [f'Mini-Batch Pegasos(k={mini_batch_size}, i.e. {mini_batch_size_percentage:.1f}%)', 'Normal Pegasos'])
     plt.savefig(f'convergence_comparison_k={mini_batch_size}')
     plt.show()
 
+# def compare_solution_accuracy(iterations, ):
--- pegasos/main.py
+++ pegasos/main.py
@@ -3,51 +3,11 @@ import matplotlib.pyplot as plt
 
 from data import read_and_relabel_train_data
 
-def plot_loss(loss, batch_size):
-    plt.figure()
-    plt.title(f'Loss convergence; batch size = {batch_size}, lambda = 1, 1000 iterations')
-    plt.xlabel("Iteration")
-    plt.ylabel("Loss")
-    plt.plot(loss)
-    plt.show()
-
-def iterations_required_plot(k, iters):
-    plt.figure()
-    plt.title(f'Iterations required to obtain a loss of 0.25 w.r.t to the batch size')
-    plt.xlabel('Batch Size')
-    plt.ylabel('Iterations')
-    plt.bar(k, iters)
-    plt.show()
-
 if __name__ == '__main__':
     training_data = read_and_relabel_train_data('MNIST-13.csv', 1)
+
+    print('Running loss convergence depending on batch size')
+    experiments.loss_convergence_depending_on_batch_size(desired_loss=0.25, training_data=training_data)
     
-    print('Running loss convergence comparison')
-    experiments.compare_loss_convergence(0.25, training_data, 50)
-
-    
-    """
-    training_data = read_and_relabel_train_data('MNIST-13.csv', 1)
-    params = PegasosParameters(lmbda=1, iterations=1000)
-    model = PegasosSolver(params)
-    loss = model.train(training_data)
-    plot_loss(loss, 'Full')
-
-    params = PegasosParameters(lmbda=1, batch_size = 30, iterations=1000)
-    model = PegasosSolver(params)
-    loss = model.train(training_data)
-    plot_loss(loss, 30)
-    """
-    
-
-    """
-    iters = []
-    batch_sizes = []
-    for k in range(1, 300, 10):
-        params = PegasosParameters(lmbda=1, batch_size = k, iterations=10000000000, loss_threshold = 0.5)
-        model = PegasosSolver(training_data.labels, params)
-        iters.append(len(model.train(training_data)))
-        batch_sizes.append(k)
-
-    iterations_required_plot([str(b) for b in batch_sizes], iters)
-    """
+    #print('Running loss convergence comparison')
+    #experiments.compare_loss_convergence(0.25, training_data, 50)
